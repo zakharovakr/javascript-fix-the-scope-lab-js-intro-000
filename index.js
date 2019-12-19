228873a49e07392070c3020f87f39b60a35fee38
Kristina Zakharova <zakharova.kr@gmail.com>
2019-12-19 19:17:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,8 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
-  const two = 2
+  const two = 2;
+  return n + two;
 }
 
 var funkyFunction = function() {
